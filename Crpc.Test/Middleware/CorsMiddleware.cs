72464ec39aceaa771fc833ec7d87271b21324176
Alexander Forbes-Reed <git@alx.red>
2020-04-26 13:55:30 +0100

    Write CorsMiddleware tests

--- Crpc.Test/Middleware/CorsMiddleware.cs
+++ Crpc.Test/Middleware/CorsMiddleware.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
 using Xunit;
 
 namespace Crpc.Test.Middleware
@@ -19,6 +20,64 @@ namespace Crpc.Test.Middleware
 		public CorsMiddlewareTests()
 		{
 			_loggerFactory = new NullLoggerFactory();
+		}
+
+		[Fact]
+		public async Task TestCorsHeadersAreSet()
+		{
+			var middleware = new CorsMiddleware(_loggerFactory);
+			var context = new DefaultHttpContext();
+
+			context.Response.Body = new MemoryStream();
+
+			await middleware.InvokeAsync(context, (ctx) =>
+			{
+				var headers = new string[] { "Authorization", "Content-Type", "Accept" };
+
+				Assert.Equal("*", GetFirstHeaderValue(ctx.Response.Headers, "Access-Control-Allow-Origin"));
+				Assert.Equal(headers, GetHeaderValues(ctx.Response.Headers, "Access-Control-Allow-Headers"));
+				Assert.Equal("POST", GetFirstHeaderValue(ctx.Response.Headers, "Access-Control-Allow-Methods"));
+
+				Console.WriteLine(GetFirstHeaderValue(ctx.Response.Headers, "Access-Control-Allow-Headers"));
+
+				return Task.CompletedTask;
+			});
+		}
+
+		[Theory]
+		[InlineData("OPTIONS", false)]
+		[InlineData("POST", true)]
+		public async Task TestSkipsOnOptions(string method, bool shouldBeCalled)
+		{
+			var middleware = new CorsMiddleware(_loggerFactory);
+			var context = new DefaultHttpContext();
+			var called = false;
+
+			context.Request.Method = method;
+			context.Response.Body = new MemoryStream();
+
+			await middleware.InvokeAsync(context, (ctx) =>
+			{
+				called = true;
+
+				return Task.CompletedTask;
+			});
+
+			Assert.Equal(shouldBeCalled, called);
+		}
+
+		private StringValues GetHeaderValues(IHeaderDictionary headers, string header)
+		{
+			headers.TryGetValue(header, out var values);
+
+			return values;
+		}
+
+		private string GetFirstHeaderValue(IHeaderDictionary headers, string header)
+		{
+			var values = GetHeaderValues(headers, header);
+
+			return values[0];
 		}
 	}
 }
